bcb2eb46132d580eebb66d519570d48cac6379a7
Manuel <carlosd99m@gmail.com>
2021-03-11 22:38:52 -0600

    Widget FutureBuilder #6 - Fin

---
+++ flutter_futurebuilder/lib/models/reqres_model.dart
@@ -0,0 +1,97 @@
+// To parse this JSON data, do
+//
+//     final reqResRespuesta = reqResRespuestaFromJson(jsonString);
+
+import 'dart:convert';
+
+ReqResRespuesta reqResRespuestaFromJson(String str) => ReqResRespuesta.fromJson(json.decode(str));
+
+String reqResRespuestaToJson(ReqResRespuesta data) => json.encode(data.toJson());
+
+class ReqResRespuesta {
+    ReqResRespuesta({
+        this.page,
+        this.perPage,
+        this.total,
+        this.totalPages,
+        this.data,
+        this.support,
+    });
+
+    int page;
+    int perPage;
+    int total;
+    int totalPages;
+    List<Usuario> data;
+    Support support;
+
+    factory ReqResRespuesta.fromJson(Map<String, dynamic> json) => ReqResRespuesta(
+        page: json["page"],
+        perPage: json["per_page"],
+        total: json["total"],
+        totalPages: json["total_pages"],
+        data: List<Usuario>.from(json["data"].map((x) => Usuario.fromJson(x))),
+        support: Support.fromJson(json["support"]),
+    );
+
+    Map<String, dynamic> toJson() => {
+        "page": page,
+        "per_page": perPage,
+        "total": total,
+        "total_pages": totalPages,
+        "data": List<dynamic>.from(data.map((x) => x.toJson())),
+        "support": support.toJson(),
+    };
+}
+
+class Usuario {
+    Usuario({
+        this.id,
+        this.email,
+        this.firstName,
+        this.lastName,
+        this.avatar,
+    });
+
+    int id;
+    String email;
+    String firstName;
+    String lastName;
+    String avatar;
+
+    factory Usuario.fromJson(Map<String, dynamic> json) => Usuario(
+        id: json["id"],
+        email: json["email"],
+        firstName: json["first_name"],
+        lastName: json["last_name"],
+        avatar: json["avatar"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "id": id,
+        "email": email,
+        "first_name": firstName,
+        "last_name": lastName,
+        "avatar": avatar,
+    };
+}
+
+class Support {
+    Support({
+        this.url,
+        this.text,
+    });
+
+    String url;
+    String text;
+
+    factory Support.fromJson(Map<String, dynamic> json) => Support(
+        url: json["url"],
+        text: json["text"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "url": url,
+        "text": text,
+    };
+}
